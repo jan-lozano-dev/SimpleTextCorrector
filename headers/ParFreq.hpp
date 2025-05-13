77b03720dc9c2b006c5d22d1e4dbea7a2e08943e
Jan Lozano <jan.lozano.dev@gmail.com>
2025-05-13 19:52:18 +0200

    Header ParFreq done, needs revision

--- headers/ParFreq.hpp
+++ headers/ParFreq.hpp
@@ -1,69 +1,71 @@
 #ifndef PARFREQ_HPP
 #define PARFREQ_HPP
 #include <string>
+#include <utility>
 using namespace std;
 
 class ParFreq {
 // Tipus de mòdul: dades
 // Descripció del tipus: Representa una paraula amb la seva freqüència
 //						d'aparició en un corpus gran. 
-
-	public:
-	
-		// ESPECIFICACIÓ DE LA CLASSE ParFreq
-		// (especificació Pre/Post de tots els mètodes)
-	
-	   //*********************************************************
-	   // Constructors
-	   //*********************************************************
-	   /*Pre: Cert*/
-	   /*Post: El resultat és un ParFreq sense cap element.*/
-	   ParFreq();
-	   //...
-
-	   /*CAL MÉS CONSTRUCTORS'?????*/
-
-
-	   //*********************************************************
-	   // Destructor
-	   //*********************************************************
-	   /*Esborra automàticament els objectes locals en sortir d'un `àmbit de
-    	     visibilitat.*/	   
-           ~ParFreq();
-	 
-   	   //*********************************************************
-	   // Modificadors
-	   //*********************************************************
-	   
-	   // ???
-	   
-	   //*********************************************************
-	   // Consultors
-	   //*********************************************************
-	   
-	   /* Pre: Cert */
-	   /* Post: El resultat és la paraula del paràmetre implícit */
-	   string getParaula() const;
-	   
-	   /* Pre: Cert */
-	   /* Post: El resultat és la freqüència del paràmetre implícit */
-	   int getFrequencia() const;
-	   
-	   /* Pre:  Cert  */
-	   /* Post: El resultat indica si la paraula del paràmetre implícit
-		és igual a la paraula del parell rebut per paràmetre */ 	   
-	   bool operator==(const ParFreq pf) const;
-	   
-	   //...
 	   
 	   
 	private:
 		// IMPLEMENTACIÓ DE LA CLASSE ParFreq 
 		// (definició del nom i tipus de cada atribut)
-		struct item {
-			string paraula;
-			int freq;
-		};
-	/*MÈTODES PRIVATS!*/
+		pair<string, int> dades;
+
+	public:	
+		//*********************************************************
+		// Constructors
+	  	//*********************************************************
+	  	/*Pre: Cert*/
+	  	/*Post: El resultat és un ParFreq sense cap element.*/
+	  	ParFreq();
+		/* Pre: Cert */
+    		/* Post: el paràmetre implícit és una copia profunda de la ParFreq donada com a paràmetre. */	
+		ParFreq(const ParFreq<T> &pf);
+		
+		/* Pre: Cert */
+		/* Post: El paràmetre implícit és una ParFreq amb s com a paraula i amb n com freqüència. */
+		ParFreq(const string &s, const int &n);
+		
+		//*********************************************************
+		// Destructor
+		//*********************************************************
+		/*Esborra automàticament els objectes locals en sortir d'un `àmbit de
+		visibilitat.*/	   
+		~ParFreq();
+		
+		//*********************************************************
+		// Modificadors
+		//*********************************************************
+		/* Pre: cert*/
+		/* Post: es substitueix el paràmetre implícit paraula per s.*/
+		void insertParaula(const string &s);
+		
+		/* Pre: cert*/
+		/* Post: es substitueix el paràmetre implícit freq per n.*/
+		void insertFreq(const int &n);
+		   
+		//*********************************************************
+		// Consultors
+		//*********************************************************
+		
+		/* Pre: Cert */
+		/* Post: El resultat és la paraula del paràmetre implícit */
+		string getParaula() const;
+		
+		/* Pre: Cert */
+		/* Post: El resultat és la freqüència del paràmetre implícit */
+		int getFrequencia() const;
+		
+		/* Pre:  Cert  */
+		/* Post: El resultat indica si la paraula del paràmetre implícit
+		és igual a la paraula del parell rebut per paràmetre */ 	   
+		bool operator==(const ParFreq pf) const;
+		
+		//...
+
 };
 #endif

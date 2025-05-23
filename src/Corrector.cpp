9208d64772c3d97410698168269b06a87b183146
Nil-Muriach <nil.muriach@estudiantat.upc.edu>
2025-05-23 18:19:30 +0200

    Update Corrector.cpp

--- src/Corrector.cpp
+++ src/Corrector.cpp
@@ -4,10 +4,43 @@ using namespace std;
 // IMPLEMENTACIÓ DE LA CLASSE Corrector
 // (implementació de tots els mètodes especificats en el fitxer Corrector.hpp)
 
-// ...
 
 // ...
+/* Pre: Cert */
+/* Post: Si rutaDiccionari està associat a un fitxer, llegeix
+les entrades del fitxer i omple el diccionari del corrector
+que crea; altrament, mostra un missatge d'error */
+Corrector(const string &rutaDiccionari) { // carrega el diccionari (BST)
+	Diccionari Dicc;
+	Dicc.llegeixDeFitxer(&rutaDiccionari);
+	v = Dicc.getVector();
+	Dicc.omplir_BTS(v);
+}
 
+
+/* Pre: Cert */
+/* Post: Si rutaInput està associat a un fitxer, llegeix el
+ text del fitxer línia a línia, corregeix cadascuna de les
+ paraules de cada línia, les escriu al fitxer associat a
+ rutaOutput i escriu al fitxer associat a rutaLog els canvis
+ que hagi fet; altrament, mostra un missatge d'error */		
+void processaText(const string &rutaInput, const string &rutaOutput, const string &rutaLog) {
+	jhj
+}
+		
+// ...
+
+//*********************************************************
+//Lectura i escriptura
+//*********************************************************		
+
+/* Pre: Cert */
+/* Post: S'han escrit al fitxer associat a rutaLog totes 
+les correccions fetes al text d'entrada sent el format de
+cada línia paraula_original -> paraula_corregida */
+void bolcaRegistre(const string &rutaLog) {
+	
+}
 
 //*********************************************************
 void Corrector::bolcaRegistre(const string &rutaLog)

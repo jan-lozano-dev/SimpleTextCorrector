749fef52a35dee6ff290cf42dc3c706f3d588509
Jan Lozano <jan.lozano.dev@gmail.com>
2025-05-28 16:20:07 +0200

    Update ParFreq.cpp

--- src/ParFreq.cpp
+++ src/ParFreq.cpp
@@ -1,54 +1,93 @@
+#include <iostream>
 #include "ParFreq.hpp"
 using namespace std;
 
-//Constructor buït
+/*Pre: Cert*/
+/*Post: El resultat és un ParFreq sense cap element.*/
 ParFreq::ParFreq(){
   dades = make_pair("", 0);
 }
 
-//Constructor per còpia
+/* Pre: Cert */
+/* Post: el paràmetre implícit és una copia profunda de la ParFreq donada com a paràmetre. */	
 ParFreq::ParFreq(const ParFreq &pf){
   dades = pf.dades;
 }
 	
-//Constructor amb inputs
+/* Pre: Cert */
+/* Post: El paràmetre implícit és una ParFreq amb s com a paraula i amb n com freqüència. */
 ParFreq::ParFreq(const string &s, const int &n){
   dades = make_pair(s, n);
 }
 
-//Destructor
+/*Esborra automàticament els objectes locals en sortir d'un `àmbit de visibilitat.*/	   
 ParFreq::~ParFreq(){
   //(no s'empra memòria dinàmica, no fem res)
 }
-//Modificador inserta nova paraula
+
+/* Pre: cert*/
+/* Post: es substitueix el paràmetre implícit paraula per s.*/
 void ParFreq::insertParaula(const string &s){
   dades.first = s;
 }
 
-//Modificador inserta nova freqüència
+/* Pre: cert*/
+/* Post: es substitueix el paràmetre implícit freq per n.*/
 void ParFreq::insertFreq(const int &n){
   dades.second = n;
 }
 
-//Consultor: retorna pair
+/* Pre: Cert */
+/* Post: El resultat és el pair del paràmetre implícit */
 pair<string, int> ParFreq::getPair() const{
   return dades;
 }
 
-//Consultor: retorna paraula del pair
+/* Pre: Cert */
+/* Post: El resultat és la paraula del paràmetre implícit */
 string ParFreq::getParaula() const{
   return dades.first;
 }
-//Consultor: retorna freqüència del pair
+
+/* Pre: Cert */
+/* Post: El resultat és la freqüència del paràmetre implícit */
 int ParFreq::getFrequencia() const{
   return dades.second;
 }
 
-//Operador comparador. Comprova si paràmetre implícit y pair pf són iguals.
+/* Pre:  Cert  */
+/* Post: El resultat indica si la paraula del paràmetre implícit
+és igual a la paraula del parell rebut per paràmetre */ 	   
 bool ParFreq::operator==(const ParFreq& pf) const{
   return dades == pf.dades;
 }
 
+/* Pre:  Cert  */
+/* Post: El resultat indica si la paraula del paràmetre implícit
+és més petita a la paraula del parell rebut per paràmetre */ 	
 bool ParFreq::operator<(const ParFreq& pf) const {
   return dades.first < pf.dades.first;
 }
+
+/* Pre:  Cert  */
+/* Post: El resultat indica si la paraula del paràmetre implícit
+és més gran a la paraula del parell rebut per paràmetre */
+bool ParFreq::operator>(const ParFreq& pf) const{
+    return dades.first > pf.dades.first;
+}
+
+/* Pre:  Cert  */
+/* Post: El  p.i. té ara la mateixa paraula i freqüència que el paràmetre `other`;
+  retorna una referència al p.i. per paràmetre. */
+ParFreq& ParFreq::operator=(const ParFreq& pf){
+  if(this != &pf){
+    dades = pf.dades;
+  }
+  return *this;
+}
+
+
+
+
+
+
